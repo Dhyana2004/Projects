5e2d19e464cc2bfd1ed2ab9aa954f3cc5cdf7bf2
K DHYANA SAMAGA <156940188+Dhyana2004@users.noreply.github.com>
2024-06-30 17:12:17 +0530

    QrCode_generator.py

---
+++ QrCode_generator.py
@@ -0,0 +1,5 @@
+# pip install qrcode (past this command in your terminal before running his program)
+
+import qrcode
+qr_image=qrcode.make('#past the text or anyting u want to conver this to qr') #eg: my name is dhyana
+qr_image.save('#the name of the file.extension') #eg:qr-img.jpg
